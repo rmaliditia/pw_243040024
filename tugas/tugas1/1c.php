a9f019ccc5b4fa3b75fc0b8788edaf9a8ed79dc8
rmaliditia <aliditia123@gmail.com>
2025-02-12 11:40:30 +0700

    Selesai mengerjakan modul 1

---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1c</title>
+
+    <style>
+        .container {
+            display: flex;
+        }
+
+        .kotak {
+            width: 50px;
+            height: 50px;
+            background-color: salmon;
+            border: .5px solid black;
+            text-align: center;
+            line-height: 50px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="kotak">1</div>
+    </div>
+    <div class="container">
+        <div class="kotak">2</div>
+        <div class="kotak">2</div>
+    </div>
+    <div class="container">
+        <div class="kotak">3</div>
+        <div class="kotak">3</div>
+        <div class="kotak">3</div>
+    </div>
+</body>
+
+</html>
