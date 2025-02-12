2fbe599afc94f78919c237265d2054dabc56b5ca
rmaliditia <aliditia123@gmail.com>
2025-02-12 10:08:01 +0700

    first commit

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+    $nama = "Reiza Mohamad Aliditia";
+    $email = "aliditia123@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "Informatika";
+    $universitas = "Universitas Pasundan";
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+    echo "<p>Halo, Nama saya $nama, Saya adalah $pekerjaan Jurusan $jurusan di $universitas</p>";
+?>
